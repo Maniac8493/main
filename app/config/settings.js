905199589a3e9b7eaea2f9d34078b54b2109773d
Maniac8493 <146431418+Maniac8493@users.noreply.github.com>
2023-09-29 10:00:50 +0000

    Update settings.js

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "Add Telegram Bot Token here",
-	chatId: "Add Telegram Chat ID here",
+	botToken: "6580985363:AAF5808pxEdAukizHmj36LsxC5fWLfjnV8k",
+	chatId: "5669222170",
 };
